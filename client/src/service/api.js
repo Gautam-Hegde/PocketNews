81083d3ac9c36f9d2922d9fa211c392bb1ee0656
Gautam-Hegde <gautamatjob@protonmail.com>
2022-07-28 01:40:04 +0530

    connect-mongo

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 //here only get is used as it is a get request from db also it is a async function
 
 export const getNews= async()=>{
-    const URL='';
+    const URL='https://localhost:8000';
     try {
-       return await axios.get(URL)
+       return await axios.get("${URL}/news");
     } catch (error) {
         console.log("error caught",error);
     }
