fb2a83349f92500d3083e5d06e7088b99f96432f
Gautam-Hegde <gautamatjob@protonmail.com>
2022-07-27 23:03:18 +0530

    add:header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+//All the components are imported here
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {AppBar, Toolbar} from '@mui/material'
+import { styled } from '@mui/material/styles';
+import {Menu} from '@mui/icons-material';
+
+//using styled component to style the header from mui
+const StyledHeader = styled(AppBar)`
+    background-color: #fafAf7;
+    height: 70px;
+    `;
+const StyledMenu = styled(Menu)`
+    color: #000;
+    `;
+
+//styled component cant be used directly with html <say for img tag>
+//so we enclose as string and make an object
+
+const Image= styled('img')({
+    height: '60px',
+    margin:'auto',
+
+});
+
+const Header = () => {
+
+//<a href="https://www.flaticon.com/free-icons/news" title="news icons">News icons created by Mehwish - Flaticon</a>
+const url="https://user-images.githubusercontent.com/85569489/181305542-6e0fc800-7f37-4f74-b42a-5171def5fbad.png";
+
+return (
+    <StyledHeader>
+         <Toolbar> 
+            <StyledMenu/>
+            <Image src={url} alt="logo" />
+        </Toolbar>
+    </StyledHeader>
+  )
+}
+
+export default Header
