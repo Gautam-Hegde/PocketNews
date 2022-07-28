6d7f45d2b6ce7763ca1566d070c699555d004e88
Gautam-Hegde <gautamatjob@protonmail.com>
2022-07-28 21:14:14 +0530

    fixes

--- server/client/src/service/api.js
+++ server/client/src/service/api.js
@@ -1,7 +1,9 @@
 //using axios to make http requests instea of fetch
 import axios from 'axios';
 //here only get is used as it is a get request from db also it is a async function
+//heroku setup pending
 const URL='';
+//use http://localhost:8000/ for local dev test
 export const getNews= async()=>{
     
     try {
