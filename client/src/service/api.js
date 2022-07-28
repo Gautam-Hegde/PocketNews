c90037111b776652cfc53397238d0910fd1253e9
Gautam-Hegde <gautamatjob@protonmail.com>
2022-07-28 17:53:30 +0530

    fix:axios error

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,11 +1,11 @@
 //using axios to make http requests instea of fetch
 import axios from 'axios';
 //here only get is used as it is a get request from db also it is a async function
-
+const URL='http://localhost:8000';
 export const getNews= async()=>{
-    const URL='https://localhost:8000';
+    
     try {
-       return await axios.get("${URL}/news");
+       return await axios.get(URL+'/news');
     } catch (error) {
         console.log("error caught",error);
     }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 import  express  from "express";
 import Connection from "./database/db.js";
 import DefaultData from "./default.js";
-
+import Route from "./routes/route.js";
+import cors from "cors";
 
 const app=express();
+app.use(cors());
+
+app.use('/',Route);
+
 const PORT=8000;
 
 Connection();
